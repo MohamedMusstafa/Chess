f62314d4b00017c51b3de07084b81bf3a1cb1147
MohamedMusstafa <mohamedmostafa1506@gmail.com>
2020-03-31 09:13:57 +0200

    Trying Js and Typing Chess Task Priorties

---
+++ js/GameEngine.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	//Chess Part Properties (PartName, pictureUrl, className, Coordinates(x, y))
+	//set postion for each (x,y) with numbers and vowels
+	//set defualt positioning for each Chess part from by numbers and vowels that you have set before
+	//specify movment abilities
+	//specify killing abilities
+	
+
+	$('.blackQueen').css('transform','translate(503px, -63px)');
+
+});
