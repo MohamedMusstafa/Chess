d1a208a66a7f77db8a30159bf1646e79807c56d8
MohamedMusstafa <mohamedmostafa1506@gmail.com>
2020-04-01 06:13:30 +0200

    Coords A1 to A8 has been added

--- js/GameEngine.js
+++ js/GameEngine.js
@@ -9,14 +9,25 @@ $(document).ready(function(){
 
 	$('.blackQueen').css('transform','translate(503px, -63px)');
 
-	// $items = {
-	// 	"board":[
-	// 	    {"firstName":"John", "lastName":"Doe"},
-	// 	    {"firstName":"Anna", "lastName":"Smith"},
-	// 	    {"firstName":"Peter", "lastName":"Jones"}
-	// 	]
-	// };
+	Coords = {
+		
+		"A":[
+		    
+		    {
+		    	'A1':['703px', '-529px'],
+		    	'A2':['703px', '-462px'],
+		    	'A3':['703px', '-395px'],
+		    	'A4':['703px', '-329px'],
+		    	'A5':['703px', '-263px'],
+		    	'A6':['703px', '-196px'],
+		    	'A7':['703px', '-130px'],
+		    	'A8':['703px', '-63px']
+			}
+		]
+	};
 
-	// console.log($items);
+	//A8 703px, 
+
+	console.log(Coords);
 
 });
